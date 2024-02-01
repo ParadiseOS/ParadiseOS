f4a3d4b3411cf6741a48c4ab4898954d60f2a58a
Joyal Mathew <joyal.mathew2023@gmail.com>
2024-02-01 17:27:39 -0500

    Migrate to using fasm and intel syntax

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM debian:bullseye-slim
 
-RUN apt update && apt install -y wget grub-pc-bin xorriso build-essential \
+RUN apt update && apt install -y wget grub-pc-bin xorriso build-essential fasm \
     bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo
 
 WORKDIR /usr
--- docker-include/build.sh
+++ docker-include/build.sh
@@ -8,7 +8,7 @@ for file in src/*.c; do
     i686-elf-gcc -c "$file" -o "bin/$(basename -s .c $file).o" -std=gnu99 -ffreestanding -g -Wall -Wextra
 done
 
-i686-elf-as src/boot.s -o bin/boot.o
+fasm src/boot.s bin/boot.o
 i686-elf-gcc -T src/linker.ld -o build/paradise-os.bin -ffreestanding -nostdlib bin/*.o -lgcc
 
 if ! grub-file --is-x86-multiboot build/paradise-os.bin; then
