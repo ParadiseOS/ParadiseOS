0cfb74806c618f9ffafb99608f2bc9b09246ab69
lk1422 <lk3ond@gmail.com>
2024-02-18 20:52:10 -0500

    INITIALIZED GDTgit branchgit branch

--- src/kernel.c
+++ src/kernel.c
@@ -1,7 +1,13 @@
 #include "terminal.h"
 #include "multiboot.h"
+#include "init.h"
 
 extern const u32 *multiboot_info;
+extern const u32 *stack_top;
+extern const u32 *_start;
+
+extern GdtEntry gdt[3];
+
 
 void kernel_main(void) {
     usize width = multiboot_info[MB_FRAME_BUFFER_WIDTH_OFFSET];
@@ -10,33 +16,13 @@ void kernel_main(void) {
 
     terminal_init(width, height, buffer);
 
+
     terminal_printf("Hello, Paradise!\n");
-    terminal_printf("Multiboot flags: %b\n", multiboot_info[MB_FLAGS_OFFSET]);
-
-    terminal_printf("Charset:\n");
-    terminal.color = vga_color_create(VGA_COLOR_LIGHT_BLUE, VGA_COLOR_BLACK);
-    for (int h = 0; h < 16; ++h) {
-        for (int l = 0; l < 16; ++l) {
-            terminal_printf("%c", h << 4 | l);
-        }
-        terminal_printf("\n");
-    }
-    terminal.color = vga_color_create(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-
-    terminal_printf("Colors:\n");
-    for (int c = 0; c < 16; ++c) {
-        terminal.color = vga_color_create(VGA_COLOR_LIGHT_GREY, c);
-        terminal_printf(" ");
-    }
-
-    i32 a = -45;
-    u32 b = 70;
-    const char *message = "Hello!";
-
-    terminal.color = vga_color_create(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    terminal_printf("\n");
-    terminal_printf("printf Test: %u, %i, %u, %i, %x, %x, %b, %b, %s, %c, %%",
-                    a, a, b, b, a, b, a, b, message, *message);
+    terminal_printf("Multiboot flags: %x\n", multiboot_info[MB_FLAGS_OFFSET]);
+    terminal_printf("Initializing GDT\n");
+    init_gdt();
+    terminal_printf("GDT Initialized\n");
+    
 
     #ifdef TESTS_ENABLED // Test Flag should be passed to build script
         terminal.color = vga_color_create(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
