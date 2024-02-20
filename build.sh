befe6fa392fb0a7982d8a0d0d48b99a269ffdf58
Anthony Fabius <afabius05@gmail.com>
2024-02-20 14:33:49 -0500

    Powershell build script

--- build.sh
+++ build.sh
@@ -19,10 +19,7 @@ do
 done
 
 ### Builds Operating System
-dockerstr="
 docker run                                                        \
-   --mount type=bind,source=\"$(pwd)\"/build,target=/usr/app/build \
-   --mount type=bind,source=\"$(pwd)\"/src,target=/usr/app/src     \
-   -it -e TESTS_ENABLED=${TESTS_ENABLED} paradise-os"
-
-eval ${dockerstr}
+   --mount type=bind,source="$(pwd)"/build,target=/usr/app/build \
+   --mount type=bind,source="$(pwd)"/src,target=/usr/app/src     \
+   -it -e TESTS_ENABLED=${TESTS_ENABLED} paradise-os
