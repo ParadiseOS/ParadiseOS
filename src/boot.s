927eee7a5187fac38541c3afb5de77b349c17b2a
lk1422 <lk3ond@gmail.com>
2024-04-12 05:52:54 -0400

    Initialized First TSS

--- src/boot.s
+++ src/boot.s
@@ -81,7 +81,7 @@ load_idt:
 
 public load_gdt
 load_gdt:
-    mov eax, [esp+0x4]; get GDT address
+    mov eax, [esp+0x4]
     lgdt [eax]
     jmp 0x08:flush_gdt
 
@@ -112,3 +112,35 @@ enable_paging:
     or eax, 0x80000000
     mov cr0, eax
     ret
+
+public load_tss
+load_tss:
+    mov eax, [esp+0x4]
+    mov [eax+0x04], esp
+    mov word [eax+0x08], 0x10
+    mov edx, cr3
+    mov [eax+0x1C], edx
+    xor edx, edx
+    mov dword [eax+0x20], check_initialized
+    pushf
+    pop edx
+    mov dword [eax+0x24], edx
+    xor edx, edx
+    mov dword [eax+0x28], 1
+    mov [eax+0x2C], ecx
+    mov [eax+0x30], edx
+    mov [eax+0x34], ebx
+    mov [eax+0x38], esp
+    mov [eax+0x3C], ebp
+    mov [eax+0x40], esi
+    mov [eax+0x44], edi
+    mov [eax+0x48], ES
+    mov [eax+0x4C], CS
+    mov [eax+0x50], SS
+    mov [eax+0x54], DS
+    mov [eax+0x58], FS
+    mov [eax+0x5C], GS
+    jmp 0x28:0x0
+check_initialized:
+    ret
+
---
+++ src/processes.h
@@ -0,0 +1,48 @@
+#ifndef PROCESS_H_
+#define PROCESS_H_
+
+#include "types.h"
+
+typedef struct __attribute__((packed, aligned(4096))) {
+    u16 prev_task_link;
+    u16 reserved1;
+    u32 esp0;
+    u16 ss0;
+    u16 reserved2;
+    u32 esp1;
+    u16 ss1;
+    u16 reserved3;
+    u32 esp2;
+    u16 ss2;
+    u16 reserved4;
+    u32 page_directory;
+    u32 eip;
+    u32 EFLAGS;
+    u32 eax;
+    u32 ecx;
+    u32 edx;
+    u32 ebx;
+    u32 esp;
+    u32 ebp;
+    u32 esi;
+    u32 edi;
+    u16 ES;
+    u16 reserved5;
+    u16 CS;
+    u16 reserved6;
+    u16 SS;
+    u16 reserved7;
+    u16 DS;
+    u16 reserved8;
+    u16 FS;
+    u16 reserved9;
+    u16 GS;
+    u16 reserved10;
+    u16 LDT;
+    u16 reserved11;
+    u16 trap;
+    u16 io_base;
+} Tss;
+
+
+#endif
