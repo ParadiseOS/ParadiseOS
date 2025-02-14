eeaa213b39f74c24e1b9364aa01f127d33983b36
Anthony Fabius <afabius05@gmail.com>
2025-02-14 17:34:32 -0500

    PIT & cleanup

--- src/interrupts/interrupt.h
+++ src/interrupts/interrupt.h
@@ -38,7 +38,7 @@ typedef struct {
     uint32_t cr2;
     uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
     uint32_t int_no, err_code;
-    uint32_t eip, csm, eflags, useresp, ss;
+    uint32_t eip, cs, eflags, useresp, ss;
 } InterruptRegisters;
 
 void init_idt();
