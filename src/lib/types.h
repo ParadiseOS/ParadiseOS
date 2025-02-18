671e0bcdcddfcd22b3231888618415c165f7965c
Joyal Mathew <joyal.mathew2023@gmail.com>
2025-02-18 01:35:04 -0500

    physical page management implemented

--- src/lib/types.h
+++ src/lib/types.h
@@ -18,4 +18,6 @@ typedef uint8_t  bool;
 #define TRUE 1
 #define FALSE 0
 
+#define NULL ((void *) 0)
+
 #endif // TYPES_H_
--- src/memory/mem.h
+++ src/memory/mem.h
@@ -2,10 +2,13 @@
 #define MEM_H_
 
 #include "lib/types.h"
+#include "boot/multiboot.h"
 
 extern void enable_paging();
 
-void init_page_structures();
-void map_pages(u32 physical_addr, u32 virtual_addr, u32 count);
+void init_paging();
+void init_frames();
+
+void map_pages_physical(u32 physical_addr, u32 virtual_addr, u32 count);
 
 #endif // MEM_H_
