f2c9e322992451c5313c2d876a8aa55bcaf1dc76
Tripplenut <afabius05@gmail.com>
2025-10-07 14:39:43 -0400

    Register Timer Callback Syscall
    The register timer callback syscall in all its glory.

--- src/drivers/timer/timer.h
+++ src/drivers/timer/timer.h
@@ -2,8 +2,15 @@
 #define TIMER_H_
 
 #include "interrupts/interrupt.h"
+#include "lib/types.h"
+#include "syscall/syscall.h"
 
-extern void (*timer_callback)(InterruptRegisters *regs);
+extern void (*sched_callback)(InterruptRegisters *regs);
+
+// This function is here for testing until we have all the syscalls for user
+// mode schedulers.
+SyscallResult
+syscall_reg_tmr_cb(void(callback)(InterruptRegisters *regs), u32 ticks);
 
 void init_timer();
 
