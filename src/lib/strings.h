797a1fef3598b0e3b8430ced4bdeef2ce983d920
lk1422 <lk3ond@gmail.com>
2025-10-14 02:16:52 -0400

    Commit test code incase I ever return

--- src/lib/strings.h
+++ src/lib/strings.h
@@ -3,7 +3,6 @@
 
 #include "types.h" // For u32, etc.
 
-const char HEX_DIGITS[] = "0123456789ABCDEF";
 
 /**
  * @brief Writes a formatted string to a buffer, ensuring not to write more
@@ -18,6 +17,18 @@ const char HEX_DIGITS[] = "0123456789ABCDEF";
  * if the buffer had been large enough, not including the
  * null terminator.
  */
-int snprintf(char *buffer, u32 n, const char *fmt, ...);
+u32 snprintf(char *buffer, u32 n, const char *fmt, ...);
+
+/**
+ * @brief Compares the lexicongraphical value of two strings
+ *
+ * @param s1 The first buffer to be compared
+ * @param s2 The second buffer to be compared
+ * @return ret < 0 if s1 < s2, ret > 0 if s1 > s2 else ret = 0 (s1 = s2)
+ * Behavior is undefined for NULL s1, s2
+ */
+i32 strcmp(const char *s1, const char* s2);
+
+
 
 #endif // STRINGS_H_
