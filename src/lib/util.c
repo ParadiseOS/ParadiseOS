126dad23febfef9c9f74481ac7221ef96932afe1
Joyal Mathew <joyal.mathew2023@gmail.com>
2025-11-25 18:30:55 -0500

    small fixes

--- src/lib/util.c
+++ src/lib/util.c
@@ -1,10 +1,11 @@
 #include "util.h"
 
 u16 get_pid_aid(u32 pid) {
-    return pid & 0xFFFF;
+    return pid >> 16;
 }
 
 u16 get_pid_tid(u32 pid) {
-    return (pid >> 16) & 0xFFFF;
+    return pid & 0xFFFF;
 }
+
 char temp_buffer[TEMP_BUFFER_LENGTH] __attribute__((aligned(4)));
