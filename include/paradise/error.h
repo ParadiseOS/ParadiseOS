77291d29a237f23fe1fc3d4b922816841dea4cd4
Joyal Mathew <joyal.mathew2023@gmail.com>
2026-01-11 16:14:51 -0500

    global syscall errors

--- include/paradise/error.h
+++ include/paradise/error.h
@@ -5,7 +5,7 @@
 
 /**
  *  Assert that some condition holds. Log and panic otherwise. Note that logging
- *  depends on the terminal being successfully initialized.
+ *  depends on the terminal or serial port being successfully initialized.
  * */
 #define KERNEL_ASSERT(condition)                                               \
     do {                                                                       \
@@ -19,6 +19,14 @@
     } while (false)
 
 #define RESULT __attribute__((warn_unused_result)) bool
+
+#define ERR_OK            0
+#define ERR_OOM           1
+#define ERR_PID_NOT_FOUND 2
+#define ERR_INVALID_INPUT 3
+#define ERR_ENT_NOT_FOUND 4
+#define ERR_ENT_EXISTS    5
+#define ERR_OP_TOO_LARGE  6
 
 extern __attribute__((noreturn)) void panic_handler();
 
