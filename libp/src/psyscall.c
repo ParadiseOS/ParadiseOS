ac8a4c2b489b93c3f6364c559d2cde279509956f
Anthony Fabius <afabius05@gmail.com>
2026-04-28 19:24:02 -0400

    for the gh action

--- libp/src/psyscall.c
+++ libp/src/psyscall.c
@@ -3,12 +3,10 @@
 SyscallReturn psyscall(u32 num, u32 a, u32 b, u32 c, u32 d, u32 e) {
     SyscallReturn ret;
 
-    asm volatile(
-        "int 0x80"
-        : "=a"(ret.ret), "=b"(ret.err)                     // Outputs
-        : "a"(num), "b"(a), "c"(b), "d"(c), "S"(d), "D"(e) // Inputs
-        : "memory"                                         // Clobbers
-    );
+    asm volatile("int 0x80"
+                 : "=a"(ret.ret), "=b"(ret.err)
+                 : "a"(num), "b"(a), "c"(b), "d"(c), "S"(d), "D"(e)
+                 : "memory");
 
     return ret;
 }
